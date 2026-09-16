d1196bb5eaaea7f68cebe6acc83c91d62fbab774
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-09-16 08:09:49 -0700

    refactor(continuation_helper): decouple repairJson and state machine into dedicated json_repair module (fixes #137) (#142)

--- lib/flutter_agent_core.dart
+++ lib/flutter_agent_core.dart
@@ -3,6 +3,7 @@ export 'src/ai_service_cloud.dart';
 export 'src/ai_service_method_channel.dart';
 export 'src/ai_service_mock.dart';
 export 'src/continuation_helper.dart';
+export 'src/json_repair.dart';
 export 'src/agent_harness.dart';
 export 'src/agent_history_entry.dart';
 export 'src/json_utils.dart';
